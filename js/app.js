2f367c90ffcc9041c9b5a0194c130d5dd099e3ca
angiecrellin <angiecrellin@gmail.com>
2016-07-24 16:58:09 -0500

    css/style.css

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(function() {
+
+    $('#search').submit(function(event) {
+        event.preventDefault();
+        var searchTerm = $('#authorBook').val();
+        getRequest(searchTerm);
+
+
+    });
+})
+
+var getRecommends = function(searchTerm) {
+    var recommends = {
+        q: searchTerm,
+        type: 'books',
+        k: '233130-AngieCre-H73O8G3C',
+        info: 1,
+        limit: 10
+    };
+    /*var url = 'https://www.tastekid.com/api/similar?q={searchTerm}+{type}+{k}+{info}+{limit}'
+        .replace('{searchTerm}', recommends.searchTerm)
+        .replace('{type}', recommends.type)
+        .replace('{k}', recommends.k)
+        .replace('{info}', recommends.info)
+        .replace('{limit}', recommends.limit)
+
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: 'GET',
+
+
+
+    })
+    console.log(data);*/
+
+    url = 'https://www.tastekid.com/api/similar?q=';
+
+    $.getJSON(url, recommends, function(data) {
+
+        console.log(data);
+    });
+}
